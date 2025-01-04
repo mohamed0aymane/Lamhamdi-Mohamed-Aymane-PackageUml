abf4f78bd0a17e1b03c089e671962842e6f449f0
mohamed0aymane <20aymanelamhamdi01@gmail.com>
2025-01-04 12:04:01 +0100

    modification sur file exploerer

--- src/org/mql/java/controller/FileExplorer.java
+++ src/org/mql/java/controller/FileExplorer.java
@@ -4,73 +4,133 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class FileExplorer {
-	private String pathPackage;
-	private String namePackage;
-	private List<Object> files;
-	
-	public FileExplorer(String pathPackage) {
-	    this.pathPackage = pathPackage;
-	    this.files = new ArrayList<>();
-	    
-	    // Déduire le package à partir du chemin donné
-	    this.namePackage = pathPackage
-	            .replace("src/", "")   // Retirer le préfixe src/
-	            .replace("/", ".")     // Remplacer les séparateurs de dossier par des points
-	            .replace("\\", ".")    // Compatibilité Windows
-	            + ".";                 // Ajouter un point à la fin pour les sous-packages
-	}
+    private String pathPackage;
+    private String namePackage;
+    private List<Class<?>> classes;
+    private List<Class<?>> enumerations;
+    private List<Class<?>> annotations;
+    private List<Class<?>> interfaces;
+  
 
-	//Constructeur pour les sous-packages
-	private FileExplorer(String pathPackage,String packageName) {
-		this.namePackage="";
-		this.pathPackage=pathPackage;
-		this.files=new ArrayList<>();
-		
-	}
-	public List<Object> loadPackages(){
-		try{ 
-			File directory =new File(pathPackage);
-				
-				if(!directory .exists()) {
-					System.out.println("le chemin specifie n'existe pas : " +pathPackage);
-					return null;
-				}
-					File[] fileList=directory.listFiles();
-					
-					if(fileList==null || fileList.length==0) { 
-						System.out.println("Aucun fichier dans le repertoire : " +pathPackage);
-						return null ;
-					}
-					
-					for(File file:fileList) {
-						if(file.isDirectory()) {
-							//gestion recursive pour les sous-dossiers
-							String subPackage=namePackage+file.getName() + ".";
-							FileExplorer subExplorer =new FileExplorer(file.getPath(),subPackage);
-						//On stocke les classes du fichier
-						}else if(file.getName().endsWith(".java")) {
-							String className=file.getName().replace(".java", "");
-							
-								try{
-									Class<?> cls=Class.forName(namePackage + className);
-									files.add(cls);
-									
-								} catch(ClassNotFoundException e) {
-									System.out.println("Classe introuvable :" + namePackage + className);
-								}
-							
-						}
-					}
-			} catch(Exception e) {
-				e.printStackTrace();
-			}
-			
-			return files;
-		
-	}
-	
+    public FileExplorer(String pathPackage) {
+        this.pathPackage = pathPackage;
+        this.classes = new ArrayList<>();
+        this.enumerations = new ArrayList<>();
+        this.annotations = new ArrayList<>();
+        this.interfaces = new ArrayList<>();
+       
 
+        // Deduire le package à partir du chemin donne
+        this.namePackage = pathPackage
+                .replace("src/", "")   // Retirer le prefixe src/
+                .replace("/", ".")     // Remplacer les separateurs de dossier par des points
+                .replace("\\", ".")    // Compatibilite Windows
+                + ".";                 // Ajouter un point à la fin pour les sous-packages
+    }
+
+    // Constructeur pour les sous-packages
+    private FileExplorer(String pathPackage, String packageName) {
+        this.pathPackage = pathPackage;
+        this.namePackage = packageName;
+        this.classes = new ArrayList<>();
+        this.enumerations = new ArrayList<>();
+        this.annotations = new ArrayList<>();
+        this.interfaces = new ArrayList<>();
+        
+    }
+
+    public void loadPackages() {
+        try {
+            File directory = new File(pathPackage);
+
+            if (!directory.exists()) {
+                System.out.println("Le chemin specifie n'existe pas : \n  - " + pathPackage);
+                return;
+            }
+
+            File[] fileList = directory.listFiles();
+
+            if (fileList == null || fileList.length == 0) {
+                System.out.println("Aucun fichier dans le repertoire :  \n  - " + pathPackage);
+                return;
+            }
+
+            for (File file : fileList) {
+                if (file.isDirectory()) {
+                    // Gestion recursive pour les sous-dossiers
+                    String subPackage = namePackage + file.getName() + ".";
+                    FileExplorer subExplorer = new FileExplorer(file.getPath(), subPackage);
+                    subExplorer.loadPackages();
+                    mergeResults(subExplorer);
+                } else if (file.getName().endsWith(".java")) {
+                    String className = file.getName().replace(".java", "");
+                    try {
+                        Class<?> cls = Class.forName(namePackage + className);
+                        categorizeClass(cls);
+                    } catch (ClassNotFoundException e) {
+                        System.out.println("Classe introuvable : " + namePackage + className);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void mergeResults(FileExplorer subExplorer) {
+        classes.addAll(subExplorer.getClasses());
+        enumerations.addAll(subExplorer.getEnumerations());
+        annotations.addAll(subExplorer.getAnnotations());
+        interfaces.addAll(subExplorer.getInterfaces());
+        
+    }
+
+    private void categorizeClass(Class<?> cls) {
+        if (cls.isAnnotation()) {
+            annotations.add(cls);
+        } else if (cls.isEnum()) {
+            enumerations.add(cls);
+        } else if (cls.isInterface()) {
+            interfaces.add(cls);
+        } else {
+            classes.add(cls);
+        }
+    }
+
+    
+
+
+    public List<Class<?>> getClasses() {
+        return classes;
+    }
+
+    public List<Class<?>> getEnumerations() {
+        return enumerations;
+    }
+
+    public List<Class<?>> getAnnotations() {
+        return annotations;
+    }
+
+    public List<Class<?>> getInterfaces() {
+        return interfaces;
+    }
+
+
+    public void printResults() {
+        System.out.println("\nClasses trouvees :");
+        classes.forEach(cls -> System.out.println("  - " + cls.getName()));
+
+        System.out.println("\nEnumerations trouvees :");
+        enumerations.forEach(enumCls -> System.out.println("  - " + enumCls.getName()));
+
+        System.out.println("\nAnnotations trouvees :");
+        annotations.forEach(annotation -> System.out.println("  - " + annotation.getName()));
+
+        System.out.println("\nInterfaces trouvees :");
+        interfaces.forEach(interfaceCls -> System.out.println("  - " + interfaceCls.getName()));
+
+        
+    }
 }
-
--- src/org/mql/java/test/Examples.java
+++ src/org/mql/java/test/Examples.java
@@ -9,27 +9,21 @@ import org.mql.java.controller.FileExplorer;
 public class Examples {
 
 	public Examples() {
-		exp01();
+		exp02();
 	}
 	//parser  les methodes et les attributs
 	 void exp01() {
-		 String path = "org/mql/java/ui/shapes";
+		 String path = "org.mql.java.ui.shapes.Rectangle";
 		 ClassParserAndRepresentation representation = new ClassParserAndRepresentation(path);
 		 System.out.println("Les attributs : "+representation.getAttributes());
 	     System.out.println("Les methodes  : "+representation.getMethodes());
 	}
 	 
 	 void exp02() {
-		 String path = "src/org/mql/java/controller/explorer"; // Remplacez par le chemin correct vers vos fichiers Java
-	        FileExplorer explorer = new FileExplorer(path);
-	        List<Object> files = explorer.loadPackages();
-	        
-	        if (files != null) {
-	            System.out.println("Fichiers et classes détectés : ");
-	            for (Object file : files) {
-	                System.out.println(file);
-	            }
-	        } 
+ 
+		 FileExplorer explorer = new FileExplorer("src/org/mql/java");
+	        explorer.loadPackages();
+	        explorer.printResults();
 	 }
 
 	public static void main(String[] args) {
